8011e6a74dbcd3a2aa4c602e88c5ef63ca6ba6af
Lovelesh Sharma <loveleshsharma@in-lovelesh.lan>
2019-01-26 20:27:39 +0530

    generated flatbuffer files using flatc compiler and implemented serializing and deserialing of employee object

--- flatbuffer_main.go
+++ flatbuffer_main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"FlatBuffer-Go/Models"
+	"FlatBuffer-Go/fbs_utility"
+	"fmt"
+)
+
 func main() {
 
+	employee := Models.GetNewEmployee(1,"Lovelesh","Sharma","7401094877",10000)
+	flatBytes := fbs_utility.SerializeEmployee(employee)
 
+	newEmp := fbs_utility.DeSerializeEmployee(flatBytes)
+	fmt.Println(newEmp)
 }
